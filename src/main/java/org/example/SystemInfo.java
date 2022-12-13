27be5131e993c81e6a2dc530864bb783742fa710
OrangeCoding2021 <bluedevpython1234@gmail.com>
2022-12-13 18:37:45 -0500

    Initial commit

---
+++ src/main/java/org/example/SystemInfo.java
@@ -0,0 +1,13 @@
+package org.example;
+// Not used file. Have not tested yet with it removed
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
